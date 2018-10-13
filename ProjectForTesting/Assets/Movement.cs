2c328ff6b7e8bc5c26dbc0b5177002a2da958fd1
Unknown <203889@edu.p.lodz.pl>
2018-10-13 20:52:33 +0200

    first option for movement
    two buttons for hold

---
+++ ProjectForTesting/Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 0.2f;
+
+    private float speedMultiplier = 1.0f;
+    private bool IsSpeedIncreasing = false;
+
+    
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+        transform.Translate(movementSpeed * speedMultiplier, 0, 0);
+        Debug.Log(movementSpeed * speedMultiplier);
+	}
+
+    public void IncreaseSpeed()
+    {
+        speedMultiplier = 2.0f;
+    }
+    public void DecreaseSpeed()
+    {
+        speedMultiplier = 0.5f;
+    }
+    public void StopChangingSpeed()
+    {
+        speedMultiplier = 1.0f;
+    }
+}
